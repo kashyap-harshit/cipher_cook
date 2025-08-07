753a00c07e73ef89b8e37844e9af7cd517c72f97
Harshit <kashyapharshit50@gmail.com>
2025-08-08 00:48:34 +0530

    main fingerprint don

--- main.py
+++ main.py
@@ -4,19 +4,50 @@ import os
 load_dotenv()
 capture = pyshark.LiveCapture(interface='Wi-Fi', tshark_path=os.getenv("TSHARK_LOC"), display_filter='tls.handshake')
 cipher_suites = []
+cipher_string = ""
+extensions_list = []
+extensions_string = ""
+the_grand_list = []
+the_grand_string = ""
+ec_point_list = []
+ec_point_string = ""
+supported_groups_list = []
+supported_groups_string = ""
 for packet in capture.sniff_continuously():
     try:
         
 
         if hasattr(packet, 'tls'):
+            version = int(packet.tls.handshake_version, 16)
+            the_grand_list.append(str(version))
 
             for i in packet.tls.handshake_ciphersuite.all_fields:
-                cipher_suites.append(i.get_default_value())
-            for i in packet.tls.field_names:
-                print(i)
-            print(packet.tls.handshake_extension_type.all_fields)
+                cipher = int(i.get_default_value(), 16)
+                cipher_suites.append(cipher)
+            cipher_suites.sort()
+            cipher_string= '-'.join(str(c) for c in cipher_suites)
+            the_grand_list.append(cipher_string)
+
             for i in packet.tls.handshake_extension_type.all_fields:
-                print(i.get_default_value())
+                extensions_list.append(i.get_default_value())
+            extensions_list.sort()
+            extensions_string = '-'.join(str(e) for e in extensions_list)
+            the_grand_list.append(extensions_string)
+
+            for i in packet.tls.handshake_extensions_supported_group.all_fields:
+                sg = int(i.get_default_value(), 16)
+                supported_groups_list.append(sg)
+            supported_groups_list.sort()
+            supported_groups_string = "-".join(str(s) for s in supported_groups_list)
+            the_grand_list.append(supported_groups_string)
+
+            for ec in packet.tls.handshake_extensions_ec_point_format.all_fields:
+                ec_point_list.append(ec.get_default_value())
+            ec_point_list.sort()
+            ec_point_string = '-'.join(str(ec) for ec in ec_point_list)
+            the_grand_list.append(ec_point_string)
+
+            print(the_grand_list)            
             break
     except Exception as e:
         print(e)
